db6e310395ffd966029f5f29411de2e181b26691
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-10-03 00:27:56 +0200

    Timer Bugs Fixed

--- scripts/timer.js
+++ scripts/timer.js
@@ -4,19 +4,33 @@ let hoursContainer = document.querySelector('.hours h3')
 let minutesContainer = document.querySelector('.minutes h3')
 let secondsContainer = document.querySelector('.seconds h3')
 
-let hoursValue = document.querySelector('#hours-input').value
-let minutesValue = document.querySelector('#minutes-input').value
-let secondsValue = document.querySelector('#seconds-input').value
+const timerForm = document.querySelector('#timer-inputs')
 
 const startButton = document.querySelector('#start-button')
 const pauseButton = document.querySelector('#pause-button')
 const stopButton = document.querySelector('#stop-button')
 //Set the countdown
-function StartTimer() {
+timerForm.addEventListener('submit', (event) => {
+    const timerData = new FormData(timerForm)
+    const hoursValue = timerData.get('hours')
+    const minutesValue = timerData.get('minutes')
+    const secondsValue = timerData.get('seconds')
+
     hoursContainer.textContent = hoursValue
     minutesContainer.textContent = minutesValue
     secondsContainer.textContent = secondsValue
-}
-startButton.addEventListener('click', () => {
-    StartTimer()
+    function startTimer() {
+        if (secondsContainer.textContent > 0) {
+            secondsContainer.textContent--
+        } else {
+            secondsContainer.textContent = '0'
+        }
+        if (secondsContainer.textContent == 0 && minutesContainer.textContent > 0) {
+            minutesContainer.textContent--
+        }
+    }
+    const timerInterval = window.setInterval(startTimer, 1000)
+    timerForm.reset()
+    event.preventDefault()
+
 })
